81794cba1ee3dff6e6e6ec3552bf41d7d651d095
Ty Brass <tybrass@outlook.com>
2024-06-11 13:35:50 -0500

    Add env file for secrets

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("Hey, Twitch");
+const tmi = require("tmi.js");
+
+const client = new tmi.Client({
+    connection: {
+        reconnect: true,
+    },
+    channels: ["a_blind_ty"],
+    identity: {
+        username: "a_blind_ty",
+        password: process.env.TWITCH_OAUTH_TOKEN,
+    },
+});
+
+client.connect();
+
+client.on("message", (channel, tags, message, self) => {
+    console.log(`${tags["display-name"]}: ${message} in ${channel}`);
+});
